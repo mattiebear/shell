f642e901adfe3c331d0fa1cab3779db687062890
Matthew Sells <matthew.sells@bonus.ly>
2025-12-17 14:32:04 -0600

    Refactor to single string varible

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,13 @@
 use std::io::{self, Write};
 
 fn main() {
+    let mut input = String::new();
+
     loop {
         print!("$ ");
-        io::stdout().flush().unwrap();
+        input.clear();
 
-        let mut input = String::new();
+        io::stdout().flush().unwrap();
         io::stdin().read_line(&mut input).unwrap();
 
         println!("{}: command not found", input.trim());
