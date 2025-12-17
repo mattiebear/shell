95c1c2447af967d8e17ce575e7f88e5fb372b87b
Matthew Sells <matthew.sells@bonus.ly>
2025-12-17 14:26:45 -0600

    Add first input

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,11 @@
-#[allow(unused_imports)]
 use std::io::{self, Write};
 
 fn main() {
-    // TODO: Uncomment the code below to pass the first stage
     print!("$ ");
     io::stdout().flush().unwrap();
+
+    let mut input = String::new();
+    io::stdin().read_line(&mut input).unwrap();
+
+    println!("{}: command not found", input.trim());
 }
