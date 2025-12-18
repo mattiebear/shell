348a597bdbc85c9b06ca15eb594a2bc576b0b9c5
Matthew Sells <matthew.sells@bonus.ly>
2025-12-18 15:52:45 -0600

    Add echo command

--- src/main.rs
+++ src/main.rs
@@ -10,11 +10,34 @@ fn main() {
         io::stdout().flush().unwrap();
         io::stdin().read_line(&mut input).unwrap();
 
-        match input.trim() {
-            "exit" => break,
-            _ => {}
+        if let Some(command) = parse_command(input.trim().to_string()) {
+            match command {
+                Command::Echo(args) => println!("{}", args),
+                Command::Exit => break,
+            }
+        } else {
+            println!("{}: command not found", input.trim());
         }
+    }
+}
 
-        println!("{}: command not found", input.trim());
+enum Command {
+    Echo(String),
+    Exit,
+}
+
+fn parse_command(input: String) -> Option<Command> {
+    if let Some(split_index) = input.find(" ") {
+        let (name, args) = input.split_at(split_index);
+
+        match name {
+            "echo" => Some(Command::Echo(args.to_string())),
+            _ => None,
+        }
+    } else {
+        match input.as_str() {
+            "exit" => Some(Command::Exit),
+            _ => None,
+        }
     }
 }
