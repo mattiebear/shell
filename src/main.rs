1b26db7d56bbbf69a068f96b46ff048a4a3391e1
Matthew Sells <matthew.sells@bonus.ly>
2025-12-29 11:11:52 -0600

    Restructure and add type command

--- src/main.rs
+++ src/main.rs
@@ -10,10 +10,10 @@ fn main() {
         io::stdout().flush().unwrap();
         io::stdin().read_line(&mut input).unwrap();
 
-        if let Some(command) = parse_command(input.trim().to_string()) {
-            match command {
-                Command::Echo(args) => println!("{}", args),
-                Command::Exit => break,
+        if let Some(command) = Command::parse(input.trim().to_string()) {
+            match command.execute() {
+                CommandResult::Continue => continue,
+                CommandResult::Exit => break,
             }
         } else {
             println!("{}: command not found", input.trim());
@@ -21,23 +21,55 @@ fn main() {
     }
 }
 
-enum Command {
-    Echo(String),
+enum CommandResult {
+    Continue,
     Exit,
 }
 
-fn parse_command(input: String) -> Option<Command> {
-    if let Some(split_index) = input.find(" ") {
-        let (name, args) = input.split_at(split_index);
+enum Command {
+    Echo(String),
+    Exit,
+    Type(String),
+}
 
-        match name {
-            "echo" => Some(Command::Echo(args.trim().to_string())),
-            _ => None,
+impl Command {
+    fn parse(input: String) -> Option<Self> {
+        if let Some(split_index) = input.find(" ") {
+            let (name, args) = input.split_at(split_index);
+
+            match name {
+                "echo" => Some(Command::Echo(args.trim().to_string())),
+                "type" => Some(Command::Type(args.trim().to_string())),
+                _ => None,
+            }
+        } else {
+            match input.as_str() {
+                "exit" => Some(Command::Exit),
+                _ => None,
+            }
         }
-    } else {
-        match input.as_str() {
-            "exit" => Some(Command::Exit),
-            _ => None,
+    }
+
+    fn is_builtin(name: &str) -> bool {
+        matches!(name, "echo" | "type" | "exit")
+    }
+
+    fn execute(&self) -> CommandResult {
+        match self {
+            Command::Echo(args) => {
+                println!("{}", args);
+                CommandResult::Continue
+            }
+            Command::Exit => CommandResult::Exit,
+            Command::Type(args) => {
+                if Command::is_builtin(args) {
+                    println!("{} is a shell builtin", args);
+                } else {
+                    println!("{}: not found", args);
+                }
+
+                CommandResult::Continue
+            }
         }
     }
 }
